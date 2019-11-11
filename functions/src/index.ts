5bbb460bc3315a841fbf766e2bf0322bf14ff4da
Cristiano Becker <cristianobecker@gmail.com>
2019-11-11 19:05:24 -0300

    removed cache info in request

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,5 @@ const CONFIG = functions.config().any.key;
 
 export const exampleFunction = functions.region(REGION).https.onRequest((req, res) => {
     console.log('exampleFunction()');
-
-    res.set('Cache-Control', 'private, max-age=0, s-maxage=0');
     res.send('hi ' + CONFIG);
    });
